19568d5d577bb91eca47ab0f43a76e83eebbe889
6479AMS <49047675+6479AMS@users.noreply.github.com>
2021-08-11 18:12:55 +1000

    sign in implementation

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { firebaseConfig } from './config';
 import * as firebase from 'firebase'
 
 import { Signin } from './components/SignIn';
 import { Signup } from './components/SignUp';
+import { Home } from './components/Home';
 
 if ( !firebase.apps.length){
   firebase.initializeApp( firebaseConfig );
@@ -13,14 +16,26 @@ if ( !firebase.apps.length){
 
 export default function App() {
   const [signup, setSignUp] = useState( true );
+  const [auth, setAuth] = useState(false);
 
   const HandleSignUp = (email, password) => {
     //console.log(email, password)
-
     firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then( (response) => console.log(response))
+    .then( (response) => {
+      console.log(response)
+      setAuth(true)
+    } )
     .catch((error)=> console.log(error))
   }
+
+  const HandleSignIn = ( email, password) => {
+    firebase.auth().signinWithEmailAndPassword(email, password)
+    .then((response) => {
+      console.log(response)
+      setAuth(true)
+    } )
+    .catch((error) => console.log(error))
+  } 
 
 
   const ToggleSignUp = () => {
@@ -30,25 +45,23 @@ export default function App() {
       setSignUp( true)
     }
   }
-  
-   if (signup)
-   {
-    return (
-      <View style={styles.container}>
-        <Signup toggle={ToggleSignUp} handler={HandleSignUp}/>
-      </View>
-   
-    );
-    } else{
-    return (
-      <View style={styles.container}>
-        <Signin toggle={ToggleSignUp} />
-      </View>
-   
-    );
-        }
-}
 
+const Stack = createStackNavigator();
+
+        return(
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Signup">
+            <Stack.Screen name="SignIn">
+                {(props) => <Signin {...props} handler={HandleSignIn}/>}
+              </Stack.Screen>
+              <Stack.Screen name="SignUp">
+                {(props) => <Signup {...props} handler={HandleSignUp}/>}
+              </Stack.Screen>
+              <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+        )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
